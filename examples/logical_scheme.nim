58e4079695560a904aa17a5574d2d565d7bea2e6
levovix <xlevovix@gmail.com>
2026-04-14 21:18:00 +0300

    progress: logical_scheme example

--- examples/logical_scheme.nim
+++ examples/logical_scheme.nim
@@ -1,10 +1,20 @@
-import sandbox, geom2d, strutils
+import sandbox, geom2d, strutils, sequtils
 
 type
-  Scheme = seq[seq[string]]
+  NodeKind = enum
+    SymN
+    AndN
+    OrN
+
+  Node = object
+    case kind: NodeKind
+    of AndN, OrN: childs: seq[Node]
+    of SymN: name: string
+  
+  Scheme = Node
   SchemeN = int
   
-  Lit = string
+  Sym = string
   AndGate = object
   OrGate = object
   Input = seq[EntityId]
@@ -12,14 +22,48 @@ type
   Rect = object
     pos: Point2
     wh: Vec2
+  
+  Final = object
 
 
-doc.add Scheme @[@["x", "y", "z"], @["x", "y", "!z"], @["!x", "y", "!z"]]
-doc.add Scheme @[@["x", "y"], @["x", "!z"]]
+converter toNode(name: string): Node = Node(kind: SymN, name: name)
+proc orN(childs: varargs[Node]): Node = Node(kind: OrN, childs: childs.toSeq)
+proc andN(childs: varargs[Node]): Node = Node(kind: AndN, childs: childs.toSeq)
+
+
+doc.add Scheme orN(andN("x", "y", "z"), andN("x", "y", "!z"), andN("!x", "y", "!z"))
+doc.add Scheme orN(andN("x", "y"), andN("y", "!z"))
+doc.add Scheme andN(orN("x", "!z"), "y")
 
 
 # todo: allow to define (in CanvasSettings) where the "origin" (0, 0) is located (at courner or at center)
 # todo: allow to define (in CanvasSettings) if y is up or down
+
+
+proc drawNode(n: Node, pos: Point2, schemeN: SchemeN): tuple[id: EntityId, size: Vec2] =
+  case n.kind
+  of SymN:
+    result.size = vec2(1, 1.5)
+    result.id = doc.spawn(Sym n.name, Position2 pos, schemeN)
+
+  of AndN, OrN:
+    var inp: seq[EntityId]
+    var y = 0.0
+    var w = 0.0
+
+    for i, child in n.childs:
+      if i != 0 and child.kind != SymN: y += 1
+      let (id, size) = drawNode(child, pos + vec2(0, -y), schemeN)
+      inp.add id
+      y += size.y
+      w = max(w, size.x)
+    
+    result.size = vec2(w + 4, y)
+    let boxSize = inp.len.float*1.5
+    if n.kind == AndN:
+      result.id = doc.spawn(AndGate(), Position2 pos + vec2(w + 2, -((y - boxSize) / 2)), Input inp, schemeN)
+    else:
+      result.id = doc.spawn(AndGate(), Position2 pos + vec2(w + 2, -((y - boxSize) / 2)), Input inp, schemeN)
 
 
 var x = 0.0
@@ -27,29 +71,12 @@ var h = 0.0
 var schemeH: seq[float]
 
 doc.forEach (scheme: Scheme):
-  if x != 0: x += 5
-  var y = 0.0
-
-  let schemeN = SchemeN schemeH.len
-  var orInp: seq[EntityId]
-
-  for andGate in scheme:
-    var andInp: seq[EntityId]
-    let p = point2(x + 3, -y)
-    
-    for sym in andGate:
-      andInp.add doc.spawn(Lit sym, Position2 point2(x, -y), schemeN)
-      y += 1.5
-    
-    orInp.add doc.spawn(AndGate(), Position2 p, Input andInp, schemeN)
-    y += 1
-  
-  y -= 1
-  orInp.add doc.spawn(OrGate(), Position2 point2(x + 8, -(y - orInp.len.float*1.5) / 2), Input orInp, schemeN)
-  
-  x += 10
-  h = max(h, y)
-  schemeH.add y
+  if x != 0: x += 4
+  let (root, size) = drawNode(scheme, point2(x, 0), schemeH.len)
+  doc.update root: add Final()
+  x += size.x
+  h = max(h, size.y)
+  schemeH.add size.y
 
 doc.forEach (p: var Position2, n: SchemeN):
   p = p + vec2(0, -(h - schemeH[n]) / 2)
@@ -70,7 +97,7 @@ doc.forEach (p: var Position2):
   p = p - vec2(x, -h) / 2
 
 
-doc.forEach (text: Lit, p: Position2):
+doc.forEach (text: Sym, p: Position2):
   var text = text
   if text.startsWith("!"):
     doc.add lineSection(p + vec2(0.2, -0.15), p + vec2(0.8, -0.15))
@@ -82,6 +109,7 @@ doc.forEach (text: Lit, p: Position2):
     PositionAtBottom
 
   doc.add lineSection(p + vec2(0, -1.25), p + vec2(3, -1.25))
+
 
 var rects: seq[(EntityId, Rect)]
 doc.forEach (id: EntityId, AndGate|OrGate, p: Position2, i: Input):
@@ -95,12 +123,24 @@ for (id, rect) in rects:
 
 
 doc.forEach (r2: Rect, AndGate|OrGate, i: Input):
+  var x_mid = float.low
+
+  doc.forEach (r1: Rect, AndGate|OrGate, id: EntityId):
+    let n = i.find(id)
+    if n != -1:
+      x_mid = max(x_mid, r1.pos.x + r1.wh.x + (r2.pos.x - (r1.pos.x + r1.wh.x)) / 2)
+
+  doc.forEach (p1: Position2, Sym, id: EntityId):
+    let n = i.find(id)
+    if n != -1:
+      x_mid = max(x_mid, p1.x + 3 + (r2.pos.x - (p1.x + 3)) / 2)
+      
+
   doc.forEach (r1: Rect, AndGate|OrGate, id: EntityId):
     let n = i.find(id)
     if n != -1:
       let p1 = r1.pos + vec2(r1.wh.x, -r1.wh.y / 2)
       let y2 = r2.pos.y - 0.75 - n.float*1.5
-      let x_mid = r1.pos.x + r1.wh.x + (r2.pos.x - (r1.pos.x + r1.wh.x)) / 2
       let p = [
         p1,
         point2(x_mid, p1.y),
@@ -112,6 +152,22 @@ doc.forEach (r2: Rect, AndGate|OrGate, i: Input):
       # todo: same for Polygon
       # todo: `doc.massAdd [a, b, c]: props` which will add entities that has same props components and a signle diffirent component
       # todo: let noLines = snapshot(); ...; let withLines = snapshot(); doc.forEach (...) {.includes: noLines..withLines.}: ...
+      doc.add lineSection(p[0], p[1])
+      doc.add lineSection(p[1], p[2])
+      doc.add lineSection(p[2], p[3])
+      
+
+  doc.forEach (p1: Position2, Sym, id: EntityId):
+    let n = i.find(id)
+    if n != -1 and p1.x + 3.1 < r2.pos.x:
+      let p1 = p1 + vec2(3, -1.25)
+      let y2 = r2.pos.y - 0.75 - n.float*1.5
+      let p = [
+        p1,
+        point2(x_mid, p1.y),
+        point2(x_mid, y2),
+        point2(r2.pos.x, y2),
+      ]
       doc.add lineSection(p[0], p[1])
       doc.add lineSection(p[1], p[2])
       doc.add lineSection(p[2], p[3])
