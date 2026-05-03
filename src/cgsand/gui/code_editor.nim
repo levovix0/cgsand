48eda9fc7f51f5938e8caa0984a53e0959b386ab
levovix <xlevovix@gmail.com>
2026-05-04 02:18:10 +0300

    add: line number display

--- src/cgsand/gui/code_editor.nim
+++ src/cgsand/gui/code_editor.nim
@@ -9,16 +9,23 @@ import ../logic/[config, code_editor]
 type
   Colorscheme* = object
     textColor*: Color = color_fg
+    lineNumberColor*: Color = "#606070".color
 
   CodeEditorContent* = ref object of Uiobj
     file*: Property[CodeFile]
     font*: Property[Font]
     colorscheme*: Property[Colorscheme]
+    
+    breakpointBarWidth*: Property[float32] = 20'f32.property
+    lineNumberBarWidth*: Property[float32] = 20'f32.property
+    changesBarWidth*: Property[float32] = 5'f32.property
+    arrowBarWidth*: Property[float32] = 20'f32.property
 
-    arrangement*: CodeArrangement
+    arrangement: CodeArrangement
 
   CodeEditor* = ref object of Uiobj
     content*: CodeEditorContent
+
 
 proc updateArrangement(this: CodeEditorContent)
 
@@ -30,22 +37,47 @@ registerComponent CodeEditor
 registerComponent CodeEditorContent
 
 
+
+proc textOffsetX(this: CodeEditorContent): float32 =
+  this.breakpointBarWidth[] + this.lineNumberBarWidth[] + this.changesBarWidth[] + this.arrowBarWidth[]
+
+proc lineNumberBarOffsetX(this: CodeEditorContent): float32 =
+  this.breakpointBarWidth[]
+
+
 method draw*(this: CodeEditorContent, ctx: DrawContext) =
   this.drawBefore(ctx)
 
   let winRect = rect(vec2(), this.parentUiRoot.wh)
+  let textOffsetX = this.textOffsetX
+  let lineNumberBarOffsetX_r = this.lineNumberBarOffsetX + this.lineNumberBarWidth[]
 
   if this.visibility[] == visible:
-    for line in this.arrangement.lines:
-      if this.globaly + line.rect.y + line.rect.h < winRect.y: continue
-      if this.globaly + line.rect.y > winRect.y + winRect.h: continue
-      ctx.drawText((this.globalXy + ctx.offset + line.rect.xy).vec3(0), line.arrangement, this.colorscheme{}.textColor.vec4)
+    for i, line in this.arrangement.lines:
+      if this.globalY + line.rect.y + line.rect.h < winRect.y: continue
+      if this.globalY + line.rect.y > winRect.y + winRect.h: continue
+      
+      ctx.drawText(
+        (this.globalXy + ctx.offset + line.rect.xy + vec2(lineNumberBarOffsetX_r, 0)).vec3(0),
+        typeset(this.font, $(i + 1)),
+        this.colorscheme{}.lineNumberColor.vec4,
+        origin=vec2(1, 0),
+      )
+      
+      ctx.drawText(
+        (this.globalXy + ctx.offset + line.rect.xy + vec2(textOffsetX, 0)).vec3(0),
+        line.arrangement,
+        this.colorscheme{}.textColor.vec4,
+      )
 
   this.drawAfter(ctx)
 
 
 proc updateArrangement(this: CodeEditorContent) =
-  this.arrangement = this.file[].toArrangement(this.font[], this.w[])
+  let lineNumberMaxWidth = typeset(this.font, $this.file[].lines.len).layoutBounds.x
+  this.lineNumberBarWidth[] = lineNumberMaxWidth
+
+  this.arrangement = this.file[].toArrangement(this.font[], this.w[] - this.textOffsetX)
   if this.arrangement.lines.len == 0:
     this.h[] = 0
   this.h[] = this.arrangement.lines[^1].rect.y + this.arrangement.lines[^1].rect.h
@@ -79,7 +111,7 @@ method init*(this: CodeEditor) =
       color = "#202020".color
 
     - ScrollArea.new:
-      this.fill(parent, 10)
+      this.fill(parent)
 
       + this.verticalScrollbar[].UiRect:
         color = "#808080".color
@@ -92,3 +124,4 @@ method init*(this: CodeEditor) =
   
     root.updateContent()
     on currentScript.changed: root.updateContent()
+    on this.w.changed: root.updateContent()
