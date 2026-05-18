9dc93dca7257a08246bf1bb1659c8aa2488fb4ef
levovix <xlevovix@gmail.com>
2026-05-18 13:15:47 +0300

    add: text rendering in pdf

--- src/cgsand/logic/pdf/writer.nim
+++ src/cgsand/logic/pdf/writer.nim
@@ -1,11 +1,19 @@
-import std/[strutils, memfiles]
+import std/[strutils, memfiles, importutils, unicode, tables]
 import pkg/[vmath, chroma]
+import pkg/pixie/fonts
+import pkg/pixie/fontformats/opentype
+
+privateAccess(Typeface)
 
 
 const mmToPt* = 72.0 / 25.4
 
 
 type
+  PdfEmbeddedFont = object
+    typeface: Typeface
+    usedGlyphs: OrderedTable[uint16, Rune]  # glyphId → rune (for ToUnicode CMap)
+
   PdfPage* = object
     widthPt*, heightPt*: float32
     content*: string
@@ -13,9 +21,13 @@ type
     lastR, lastG, lastB, lastA: float32
     lastLW: float32
     colorSet, lwSet: bool
+    lastFR, lastFG, lastFB: float32
+    fillColorSet: bool
+    usedFonts: seq[int]
 
   PdfWriter* = object
     pages*: seq[PdfPage]
+    fonts: seq[PdfEmbeddedFont]
 
   MemFileContext* = object
     mf*: MemFile
@@ -46,22 +58,44 @@ proc ensureAlpha(p: var PdfPage; a: float32): int =
   result = p.alphas.len
   p.alphas.add a
 
+proc ensureFont(w: var PdfWriter; tf: Typeface): int =
+  if tf.filePath.len == 0: return -1
+  for i, f in w.fonts:
+    if f.typeface == tf: return i
+  w.fonts.add PdfEmbeddedFont(typeface: tf)
+  w.fonts.high
+
+
 proc setStrokeColor*(p: var PdfPage; color: Color) =
   if not p.colorSet or p.lastR != color.r or p.lastG != color.g or p.lastB != color.b:
     p.content.add pdfNum(color.r) & " " & pdfNum(color.g) & " " & pdfNum(color.b) & " RG\n"
     p.lastR = color.r; p.lastG = color.g; p.lastB = color.b
     p.colorSet = true
-  
+
   if p.lastA != color.a:
     let idx = p.ensureAlpha(color.a)
     p.content.add "/GS" & $idx & " gs\n"
     p.lastA = color.a
+
+
+proc setFillColor*(p: var PdfPage; color: Color) =
+  if not p.fillColorSet or p.lastFR != color.r or p.lastFG != color.g or p.lastFB != color.b:
+    p.content.add pdfNum(color.r) & " " & pdfNum(color.g) & " " & pdfNum(color.b) & " rg\n"
+    p.lastFR = color.r; p.lastFG = color.g; p.lastFB = color.b
+    p.fillColorSet = true
+
+  if p.lastA != color.a:
+    let idx = p.ensureAlpha(color.a)
+    p.content.add "/GS" & $idx & " gs\n"
+    p.lastA = color.a
+
 
 proc setLineWidth*(p: var PdfPage; w: float32) =
   if not p.lwSet or p.lastLW != w:
     p.content.add pdfNum(w) & " w\n"
     p.lastLW = w
     p.lwSet = true
+
 
 proc moveTo*(p: var PdfPage; pos: Vec2) =
   p.content.add pdfNum(pos.x) & " " & pdfNum(pos.y) & " m\n"
@@ -72,6 +106,7 @@ proc lineTo*(p: var PdfPage; pos: Vec2) =
 proc stroke*(p: var PdfPage) =
   p.content.add "S\n"
 
+
 proc drawLine*(p: var PdfPage; a, b: Vec2; color: Color; lw: float32) =
   p.setStrokeColor(color)
   p.setLineWidth(lw)
@@ -80,33 +115,89 @@ proc drawLine*(p: var PdfPage; a, b: Vec2; color: Color; lw: float32) =
   p.stroke()
 
 
-proc pdfCurrentPos*(o: string): int = o.len
-proc pdfSerializeWrite*(s: string,  o: var string)  = o.add s
-proc pdfSerializeWrite*(n: int,     o: var string)  = o.add $n
-proc pdfSerializeWrite*(f: float32, o: var string)  = o.add pdfNum(f)
+proc drawText*(p: var PdfPage; w: var PdfWriter;
+               typeface: Typeface; sizePt: float32;
+               color: Color; baselineX, baselineY: float32; text: string) =
+  if text.len == 0: return
+  let fontIdx = w.ensureFont(typeface)
+  if fontIdx < 0: return
 
-proc pdfCurrentPos*(o: int): int = o
-proc pdfSerializeWrite*(s: string,  o: var int) = o += s.len
-proc pdfSerializeWrite*(n: int,     o: var int) = o += ($n).len
-proc pdfSerializeWrite*(f: float32, o: var int) = o += pdfNum(f).len
+  if fontIdx notin p.usedFonts:
+    p.usedFonts.add fontIdx
 
-proc pdfCurrentPos*(o: MemFileContext): int = o.pos
-proc pdfSerializeWrite*(s: string, o: var MemFileContext) =
+  var hexGlyphs = ""
+  for rune in text.runes:
+    let gid: uint16 =
+      if typeface.opentype != nil and typeface.opentype.cmap != nil:
+        typeface.opentype.cmap.runeToGlyphId.getOrDefault(rune, 0)
+      else:
+        0'u16
+    w.fonts[fontIdx].usedGlyphs[gid] = rune
+    hexGlyphs.add toHex(gid.int, 4)
+
+  p.setFillColor(color)
+  p.content.add "BT\n"
+  p.content.add "/Ff" & $fontIdx & " " & pdfNum(sizePt) & " Tf\n"
+  p.content.add pdfNum(baselineX) & " " & pdfNum(baselineY) & " Td\n"
+  p.content.add "<" & hexGlyphs & "> Tj\n"
+  p.content.add "ET\n"
+
+
+proc pdfCurrentPos(o: string): int = o.len
+proc pdfSerializeWrite(s: string,  o: var string)  = o.add s
+proc pdfSerializeWrite(n: int,     o: var string)  = o.add $n
+proc pdfSerializeWrite(f: float32, o: var string)  = o.add pdfNum(f)
+
+proc pdfCurrentPos(o: int): int = o
+proc pdfSerializeWrite(s: string,  o: var int) = o += s.len
+proc pdfSerializeWrite(n: int,     o: var int) = o += ($n).len
+proc pdfSerializeWrite(f: float32, o: var int) = o += pdfNum(f).len
+
+proc pdfCurrentPos(o: MemFileContext): int = o.pos
+proc pdfSerializeWrite(s: string, o: var MemFileContext) =
   if s.len > 0:
     copyMem(cast[pointer](cast[int](o.mf.mem) + o.pos), unsafeAddr s[0], s.len)
   o.pos += s.len
-proc pdfSerializeWrite*(n: int,     o: var MemFileContext) = pdfSerializeWrite($n, o)
-proc pdfSerializeWrite*(f: float32, o: var MemFileContext) = pdfSerializeWrite(pdfNum(f), o)
+proc pdfSerializeWrite(n: int,     o: var MemFileContext) = pdfSerializeWrite($n, o)
+proc pdfSerializeWrite(f: float32, o: var MemFileContext) = pdfSerializeWrite(pdfNum(f), o)
 
-proc pdfSerializeWrite*(e: PdfXrefEntry, o: var string) =
+proc pdfSerializeWrite(e: PdfXrefEntry, o: var string) =
   o.add ($e.int).align(10, '0') & " 00000 n \n"
-proc pdfSerializeWrite*(e: PdfXrefEntry, o: var int) = o += 20
-proc pdfSerializeWrite*(e: PdfXrefEntry, o: var MemFileContext) =
+proc pdfSerializeWrite(e: PdfXrefEntry, o: var int) = o += 20
+proc pdfSerializeWrite(e: PdfXrefEntry, o: var MemFileContext) =
   pdfSerializeWrite(($e.int).align(10, '0') & " 00000 n \n", o)
 
 
-proc pdfSerialize*[T](w: PdfWriter, o: var T) =
+proc writeCMap[T](font: PdfEmbeddedFont; j: int; o: var T) =
+  pdfSerializeWrite("/CIDInit /ProcSet findresource begin\n12 dict begin\nbegincmap\n", o)
+  pdfSerializeWrite("/CIDSystemInfo << /Registry (Adobe) /Ordering (UCS) /Supplement 0 >> def\n", o)
+  pdfSerializeWrite("/CMapName /Ff", o)
+  pdfSerializeWrite(j, o)
+  pdfSerializeWrite("-UCS def\n/CMapType 2 def\n1 begincodespacerange\n<0000> <FFFF>\nendcodespacerange\n", o)
+  if font.usedGlyphs.len > 0:
+    pdfSerializeWrite(font.usedGlyphs.len, o)
+    pdfSerializeWrite(" beginbfchar\n", o)
+    for gid, rune in font.usedGlyphs:
+      let cp = rune.uint32
+      pdfSerializeWrite("<", o)
+      pdfSerializeWrite(toHex(gid.int, 4), o)
+      pdfSerializeWrite("> <", o)
+      if cp < 0x10000'u32:
+        pdfSerializeWrite(toHex(cp.int, 4), o)
+      else:
+        let u  = cp - 0x10000'u32
+        let hi = 0xD800'u32 + (u shr 10)
+        let lo = 0xDC00'u32 + (u and 0x3FF)
+        pdfSerializeWrite(toHex(hi.int, 4), o)
+        pdfSerializeWrite(toHex(lo.int, 4), o)
+      pdfSerializeWrite(">\n", o)
+    pdfSerializeWrite("endbfchar\n", o)
+  pdfSerializeWrite("endcmap\nCMapName currentdict /CMap defineresource pop\nend\nend\n", o)
+
+
+proc pdfSerialize[T](w: PdfWriter, o: var T) =
   var offsets: seq[int]
+  let fontBase = 3 + w.pages.len * 2
 
   pdfSerializeWrite("%PDF-1.4\n", o)
 
@@ -127,34 +218,140 @@ proc pdfSerialize*[T](w: PdfWriter, o: var T) =
     let pageN    = 3 + i * 2
     let contentN = pageN + 1
 
-    var resources = ""
-    if page.alphas.len > 0:
-      resources = " /Resources << /ExtGState <<"
-      for j, a in page.alphas:
-        resources.add " /GS" & $j & " << /Type /ExtGState /CA " & pdfNum(a) & " >>"
-      resources.add " >> >>"
-
     offsets.add pdfCurrentPos(o)
-    pdfSerializeWrite($pageN & " 0 obj\n<< /Type /Page /Parent 2 0 R /MediaBox [0 0 ", o)
+    pdfSerializeWrite(pageN, o)
+    pdfSerializeWrite(" 0 obj\n<< /Type /Page /Parent 2 0 R /MediaBox [0 0 ", o)
     pdfSerializeWrite(page.widthPt, o)
     pdfSerializeWrite(" ", o)
     pdfSerializeWrite(page.heightPt, o)
-    pdfSerializeWrite("] /Contents " & $contentN & " 0 R" & resources & " >>\nendobj\n", o)
+    pdfSerializeWrite("] /Contents ", o)
+    pdfSerializeWrite(contentN, o)
+    pdfSerializeWrite(" 0 R", o)
+    if page.alphas.len > 0 or page.usedFonts.len > 0:
+      pdfSerializeWrite(" /Resources <<", o)
+      if page.alphas.len > 0:
+        pdfSerializeWrite(" /ExtGState <<", o)
+        for j, a in page.alphas:
+          pdfSerializeWrite(" /GS", o)
+          pdfSerializeWrite(j, o)
+          pdfSerializeWrite(" << /Type /ExtGState /CA ", o)
+          pdfSerializeWrite(a, o)
+          pdfSerializeWrite(" /ca ", o)
+          pdfSerializeWrite(a, o)
+          pdfSerializeWrite(" >>", o)
+        pdfSerializeWrite(" >>", o)
+      if page.usedFonts.len > 0:
+        pdfSerializeWrite(" /Font <<", o)
+        for fontIdx in page.usedFonts:
+          pdfSerializeWrite(" /Ff", o)
+          pdfSerializeWrite(fontIdx, o)
+          pdfSerializeWrite(" ", o)
+          pdfSerializeWrite(fontBase + fontIdx * 5, o)
+          pdfSerializeWrite(" 0 R", o)
+        pdfSerializeWrite(" >>", o)
+      pdfSerializeWrite(" >>", o)
+    pdfSerializeWrite(" >>\nendobj\n", o)
 
     offsets.add pdfCurrentPos(o)
-    pdfSerializeWrite($contentN & " 0 obj\n<< /Length ", o)
+    pdfSerializeWrite(contentN, o)
+    pdfSerializeWrite(" 0 obj\n<< /Length ", o)
     pdfSerializeWrite(page.content.len, o)
     pdfSerializeWrite(" >>\nstream\n", o)
     pdfSerializeWrite(page.content, o)
     pdfSerializeWrite("\nendstream\nendobj\n", o)
 
+  for j, font in w.fonts:
+    let type0N = fontBase + j * 5
+    let cidN   = type0N + 1
+    let descN  = type0N + 2
+    let fileN  = type0N + 3
+    let toUniN = type0N + 4
+
+    let fontBytes  = readFile(font.typeface.filePath)
+    let unitsPerEm = font.typeface.scale
+    let ascent     = font.typeface.ascent    * 1000.0 / unitsPerEm
+    let descent    = font.typeface.descent   * 1000.0 / unitsPerEm
+    let capHeight  = font.typeface.capHeight * 1000.0 / unitsPerEm
+
+    offsets.add pdfCurrentPos(o)
+    pdfSerializeWrite(type0N, o)
+    pdfSerializeWrite(" 0 obj\n<< /Type /Font /Subtype /Type0 /BaseFont /Ff", o)
+    pdfSerializeWrite(j, o)
+    pdfSerializeWrite(" /Encoding /Identity-H /DescendantFonts [", o)
+    pdfSerializeWrite(cidN, o)
+    pdfSerializeWrite(" 0 R] /ToUnicode ", o)
+    pdfSerializeWrite(toUniN, o)
+    pdfSerializeWrite(" 0 R >>\nendobj\n", o)
+
+    offsets.add pdfCurrentPos(o)
+    pdfSerializeWrite(cidN, o)
+    pdfSerializeWrite(" 0 obj\n<< /Type /Font /Subtype /CIDFontType2 /BaseFont /Ff", o)
+    pdfSerializeWrite(j, o)
+    pdfSerializeWrite(" /CIDSystemInfo << /Registry (Adobe) /Ordering (Identity) /Supplement 0 >>", o)
+    pdfSerializeWrite(" /FontDescriptor ", o)
+    pdfSerializeWrite(descN, o)
+    pdfSerializeWrite(" 0 R /DW 1000 /W [", o)
+    for gid, rune in font.usedGlyphs:
+      let adv =
+        if font.typeface.opentype != nil:
+          let idx = min(gid.int, font.typeface.opentype.hmtx.hMetrics.high)
+          font.typeface.opentype.hmtx.hMetrics[idx].advanceWidth.float32 * 1000.0 / unitsPerEm
+        else:
+          1000.0
+      pdfSerializeWrite(gid.int, o)
+      pdfSerializeWrite(" [", o)
+      pdfSerializeWrite(adv, o)
+      pdfSerializeWrite("] ", o)
+    pdfSerializeWrite("] /CIDToGIDMap /Identity >>\nendobj\n", o)
+
+    offsets.add pdfCurrentPos(o)
+    pdfSerializeWrite(descN, o)
+    pdfSerializeWrite(" 0 obj\n<< /Type /FontDescriptor /FontName /Ff", o)
+    pdfSerializeWrite(j, o)
+    pdfSerializeWrite(" /Flags 4 /FontBBox [0 ", o)
+    pdfSerializeWrite(descent, o)
+    pdfSerializeWrite(" 1000 ", o)
+    pdfSerializeWrite(ascent, o)
+    pdfSerializeWrite("] /ItalicAngle 0 /Ascent ", o)
+    pdfSerializeWrite(ascent, o)
+    pdfSerializeWrite(" /Descent ", o)
+    pdfSerializeWrite(descent, o)
+    pdfSerializeWrite(" /CapHeight ", o)
+    pdfSerializeWrite(capHeight, o)
+    pdfSerializeWrite(" /StemV 80 /FontFile2 ", o)
+    pdfSerializeWrite(fileN, o)
+    pdfSerializeWrite(" 0 R >>\nendobj\n", o)
+
+    offsets.add pdfCurrentPos(o)
+    pdfSerializeWrite(fileN, o)
+    pdfSerializeWrite(" 0 obj\n<< /Length ", o)
+    pdfSerializeWrite(fontBytes.len, o)
+    pdfSerializeWrite(" /Length1 ", o)
+    pdfSerializeWrite(fontBytes.len, o)
+    pdfSerializeWrite(" >>\nstream\n", o)
+    pdfSerializeWrite(fontBytes, o)
+    pdfSerializeWrite("\nendstream\nendobj\n", o)
+
+    var cmapLen = 0
+    writeCMap(font, j, cmapLen)
+    offsets.add pdfCurrentPos(o)
+    pdfSerializeWrite(toUniN, o)
+    pdfSerializeWrite(" 0 obj\n<< /Length ", o)
+    pdfSerializeWrite(cmapLen, o)
+    pdfSerializeWrite(" >>\nstream\n", o)
+    writeCMap(font, j, o)
+    pdfSerializeWrite("\nendstream\nendobj\n", o)
+
   let xrefPos = pdfCurrentPos(o)
   let total   = 1 + offsets.len
-  pdfSerializeWrite("xref\n0 " & $total & "\n", o)
-  pdfSerializeWrite("0000000000 65535 f \n", o)
+  pdfSerializeWrite("xref\n0 ", o)
+  pdfSerializeWrite(total, o)
+  pdfSerializeWrite("\n0000000000 65535 f \n", o)
   for off in offsets:
     pdfSerializeWrite(PdfXrefEntry(off), o)
-  pdfSerializeWrite("trailer\n<< /Size " & $total & " /Root 1 0 R >>\nstartxref\n", o)
+  pdfSerializeWrite("trailer\n<< /Size ", o)
+  pdfSerializeWrite(total, o)
+  pdfSerializeWrite(" /Root 1 0 R >>\nstartxref\n", o)
   pdfSerializeWrite(xrefPos, o)
   pdfSerializeWrite("\n%%EOF\n", o)
 
--- src/cgsand/logic/pdf_renderer.nim
+++ src/cgsand/logic/pdf_renderer.nim
@@ -1,7 +1,9 @@
+import std/unicode
 import pkg/[ecs, vmath]
+import pkg/pixie/fonts as pixieFonts
 import ../logic/[doclayout, bounds]
 import ../lib/sandbox except Mat4, mat4, Vec4, Vec3, Vec2, vec2, vec3, vec4
-import ../lib/[geom2d]
+import ../lib/[geom2d, text]
 import ./pdf/writer
 
 
@@ -38,6 +40,43 @@ proc renderPdf*(r: PdfRenerer, o: var PdfWriter) =
       color,
       thickness * scale * mmToPt.float32,
     )
+
+  r.doc[].forEach (
+    text: Text,
+    pos: Position2,
+    posAt: PositionAt || PositionAtTopLeft,
+    font: Typeface || globals.font,
+    fontSize: FontSize || globals.fontSize,
+    opt Foreground, opt Color
+  ):
+    let fg =
+      if has Foreground: the Foreground
+      elif has Color: the Color
+      else: globals.foreground
+
+    let sizePt    = fontSize.float32 * scale * mmToPt.float32
+    let fnt       = font.withSize(sizePt.float64)
+    let arr       = pixieFonts.typeset(fnt, text)
+    let box       = arr.layoutBounds()
+    let origin    = posAt.factor()
+    let anchor    = toPagePos(vec2(pos.x.float32, pos.y.float32))
+
+    # top-left of text box in PDF Y-up coordinates
+    let textTopX  = anchor.x - box.x * origin.x
+    let textTopY  = anchor.y + box.y * origin.y
+
+    for (lineStart, lineStop) in arr.lines:
+      # find first non-LF rune to get baseline position
+      var firstIdx = lineStart
+      while firstIdx <= lineStop and arr.runes[firstIdx].uint32 == 10:
+        inc firstIdx
+      if firstIdx > lineStop: continue
+
+      var lineText = $arr.runes[lineStart..lineStop]
+      let baselineX = textTopX + arr.positions[firstIdx].x
+      let baselineY = textTopY - arr.positions[firstIdx].y
+
+      o.pages[pi].drawText(o, font, sizePt, fg, baselineX, baselineY, lineText)
 
 
 proc writePdf*(filename: string, r: PdfRenerer) =
